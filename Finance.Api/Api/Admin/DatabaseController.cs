7526d2295fad40978ec338d3d01c62b184658a25
Marc <marcsstevenson@hotmail.com>
2017-03-19 14:28:56 +1300

    Added database migrations controller

---
+++ Finance.Api/Api/Admin/DatabaseController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Finance.Repository.Ef.Helpers;
+
+namespace Finance.Api.Api.Admin
+{
+    [Authorize]
+    [RoutePrefix("api/Account")]
+    public class DatabaseController : ApiController
+    {
+        // GET api/Account/Test
+        [HttpGet]
+        [Route("RunUpdates")]
+        public int RunUpdates()
+        {
+            var dbMigrator = FinanceMigrationManager.GetMigrator();
+            dbMigrator.Configuration.CommandTimeout = 180;
+            var pendingMigrations = dbMigrator.GetPendingMigrations().ToList();
+
+            //Update the database if we need to
+            if (pendingMigrations.Count > 0)
+                dbMigrator.Update();
+
+            return pendingMigrations.Count;
+        }
+    }
+}
